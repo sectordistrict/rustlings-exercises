171f959005d6a73c1142b0dc84d2f9ae660a481b
district <district@email.email>
2024-02-02 17:14:28 +0000

    .

--- exercises/iterators/iterators4.rs
+++ exercises/iterators/iterators4.rs
@@ -3,9 +3,16 @@
 // Execute `rustlings hint iterators4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 pub fn factorial(num: u64) -> u64 {
+
+    match num {
+        0 => 1,
+        n => (1..=num).into_iter().rev().fold(1, |x, y| x * y),
+    }
+
+
+
     // Complete this function to return the factorial of num
     // Do not use:
     // - return
